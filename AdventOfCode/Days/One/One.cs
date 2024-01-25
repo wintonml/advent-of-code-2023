dfcf0b02118dc8fa0b8e814e7bd109020f9ab567
wintonml <wintonml@gmail.com>
2024-01-25 20:45:04 +1300

    Basic structure to project

---
+++ AdventOfCode/Days/One/One.cs
@@ -0,0 +1,4 @@
+namespace Days.One
+{
+    
+}
---
+++ AdventOfCode/Helpers/FileHelper.cs
@@ -0,0 +1,11 @@
+namespace Helpers.FileHelper
+{
+    public class FileHelper
+    {
+        public string[] GetStringArrayFromFile(string filePathway)
+        {
+            Console.WriteLine(filePathway);
+            return new string[] {"",""};
+        }
+    }
+}
