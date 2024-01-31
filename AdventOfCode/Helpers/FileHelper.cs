c3b90ea2c541b837c0d302e17c7b4609b531ff20
wintonml <wintonml@gmail.com>
2024-01-31 20:57:44 +1300

    Refactored code and added new test cases

--- AdventOfCode/Helpers/FileHelper.cs
+++ AdventOfCode/Helpers/FileHelper.cs
@@ -14,5 +14,21 @@ namespace Helpers.FileHelper
                 throw new Exception();
             }
         }
+
+        static public string ConstructFileDirectory(bool isAccessingFromTest, bool isTest, bool isPartOne, string dayNumber)
+        {
+            string section = isPartOne ? "PartOne" : "PartTwo";
+            string prefix = isAccessingFromTest ? "../../../../" : string.Empty;
+            string fileDirectory;
+            if(isTest)
+            {
+                fileDirectory = $"{prefix}Inputs/Examples/{dayNumber}/{dayNumber}{section}ExampleInput.txt";
+            }
+            else
+            {
+                fileDirectory = $"{prefix}Inputs/Puzzles/{dayNumber}/{dayNumber}{section}Input.txt";
+            }
+            return fileDirectory;
+        }
     }
 }
