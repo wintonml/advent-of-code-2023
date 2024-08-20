c60b8ae7169095a87c9a08990ed2121fbafd00b7
wintonml <wintonml@gmail.com>
2024-08-20 23:44:10 +0200

    Answer

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,23 +1,29 @@
-﻿using AdventOfCode.Days.One;
-using AdventOfCode.Helpers;
+﻿using AdventOfCode.Helpers;
 
-namespace Program
+namespace AdventOfCode
 {
     public class Program
     {
         static void Main()
         {
-            var solver = new DayOne();
+            var dayOneSolver = new Days.One.DayOne();
             var fileDirectory = FileHelper.ConstructFileDirectory(
-                isAccessingFromTest: false, isTest: false,isPartOne: true,
-                nameof(DayOne).ToString());
-            solver.SetInputUsingFileDirectory(fileDirectory);
-            Console.WriteLine("Day One Part One answer is: " + solver.PartOneSolver());
+                isAccessingFromTest: false, isTest: false, isPartOne: true,
+                nameof(Days.One.DayOne).ToString());
+            dayOneSolver.SetInputUsingFileDirectory(fileDirectory);
+            Console.WriteLine("Day One Part One answer is: " + dayOneSolver.PartOneSolver());
 
             fileDirectory = FileHelper.ConstructFileDirectory(
-                isAccessingFromTest: false, isTest: false,isPartOne: false, nameof(DayOne).ToString());
-            solver.SetInputUsingFileDirectory(fileDirectory);
-            Console.WriteLine("Day One Part Two answer is: " + solver.PartTwoSolver());
+                isAccessingFromTest: false, isTest: false, isPartOne: false, nameof(Days.One.DayOne).ToString());
+            dayOneSolver.SetInputUsingFileDirectory(fileDirectory);
+            Console.WriteLine("Day One Part Two answer is: " + dayOneSolver.PartTwoSolver());
+
+            var dayTwoSolver = new Days.Two.DayTwo();
+            fileDirectory = FileHelper.ConstructFileDirectory(
+                isAccessingFromTest: false, isTest: false, isPartOne: true,
+                nameof(Days.Two.DayTwo).ToString());
+            dayTwoSolver.SetInputUsingFileDirectory(fileDirectory);
+            Console.WriteLine("Day Two Part One answer is: " + dayTwoSolver.PartOneSolver());
         }
     }
 }
